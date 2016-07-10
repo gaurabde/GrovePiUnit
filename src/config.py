0deea65967a811f422680f11d55b44da49a89000
Gaurab Dey <gaurabde@yahoo.com>
2016-07-09 19:06:27 -0700

    few boring changes

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import yaml
 class ConfigLoad:
     def __init__(self):
         self.config_path = '../config/config.yml'
-
+        self.config = self.load_config()
     def load_config(self):
         try:
             with open(self.config_path, 'r') as config:
@@ -16,7 +16,26 @@ class ConfigLoad:
     def print_config(self):
         print self.load_config()
 
+    def get_pins(self):
+       return  self.config['grovepi_config']['pins']
+
+    def get_threshold(self):
+       return self.config['grovepi_config']['threshold']
+
+    #sound sensor pin
+    def get_ss_pin(self):
+       return self.get_pins['sound_sensor']
+    #red led pin
+    def get_rled_pin(self):
+       return self.get_pins['red_led']
+    #rgb display pin
+    def get_rgb_pin(self):
+       return self.get_pins['rbg_display']
+    #ultrasonic range finder pin
+    def get_usonic_pin(self):
+       return self.get_pins['ultrasonic_pin']
+
 
 if __name__ == '__main__':
     c = ConfigLoad()
-    c.print_config()
+    c.get_pins()
